137eeb096626e47b78a4c31caf006cbf5610219f
Andrii-Mitko <andreymit123@gmail.com>
2026-08-31 22:10:29 +0300

    Add support for the Node.js version to package.json and package-lock.json; change the order of imports in route.ts

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/db/mongodb";
 import { Category } from "@/models/Category";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
